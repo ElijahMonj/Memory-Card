ee8467c948ac5cc42cf89504b970620e4da9a338
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-10-21 11:38:09 +0800

    Finished designing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Overview from "./components/Overview";
-import uniqid from "uniqid";
+
 import nz from "./images/nz.png";
 import is from "./images/is.png";
 import au from "./images/au.png";
@@ -13,6 +13,7 @@ import pl from "./images/pl.png";
 import no from "./images/no.png";
 import nl from "./images/nl.png";
 import se from "./images/se.png";
+import gh from "./images/gh.png";
 class App extends Component {
  
   constructor() {
@@ -23,11 +24,25 @@ class App extends Component {
       currentScore: 0,
       bestScore: 0,
       selected: [],
+      country:[
+        {name:'New Zealand',flag:nz},
+        {name:'Russia',flag:ru},
+        {name:'Iceland',flag:is},
+        {name:'Sweden',flag:se},
+        {name:'Netherlands',flag:nl},
+        {name:'Poland',flag:pl},
+        {name:'Denmark',flag:dk},
+        {name:'Australia',flag:au},
+        {name:'Finland',flag:fi},
+        {name:'France',flag:fr},
+        {name:'Indonesia',flag:id},
+        {name:'Norway',flag:no},
+      ]
     };
   }
   
   select1 = (e) => {
-    const { currentScore, bestScore, selected } = this.state;
+    const { currentScore, bestScore, selected, country } = this.state;
     console.log(e);
     console.log("Selected: "+e.target.innerText);
     console.log("Score: "+currentScore);
@@ -62,11 +77,21 @@ class App extends Component {
         bol="3rd if";
         console.log("Nice")
     }
+    let newCountry = country
+    //.map(value => ({ value, sort: Math.random() }))
+    //.sort((a, b) => a.sort - b.sort)
+    //.map(({ value }) => value)
     console.log(bol)
+    if(newScore==12){
+      newScore=0;
+      newBestScore=12;
+      newSelected=[];
+    }
     this.setState({
       currentScore: newScore,
       bestScore: newBestScore,
       selected: newSelected,
+      country:newCountry,
     });
     
   };
@@ -81,70 +106,76 @@ class App extends Component {
   
 
   render() {
-    const { currentScore, bestScore } = this.state;
+    const { currentScore, bestScore, country } = this.state;
 
     return (
-      <div>
+      <div className="game">
       <header>Memory Game</header>
-      <div>Select an image, but never select it again.</div>
+      <div className="subheading">Select a country, but never select it again.</div>
+      <div className="scores">
       <p id="para">Your current Score: {currentScore}</p>
       <p>Your Best Score: {bestScore}</p>
-      <div className="grid">
-        <div id="element">
-          <div onClick={this.select1} className="name">New Zealand</div>
-          <img src={nz}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Finland</div>
-          <img src={fi}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Australia</div>
-          <img src={au}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Netherlands</div>
-          <img src={nl}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Norway</div>
-          <img src={no}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Denmark</div>
-          <img src={dk}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Russia</div>
-          <img src={ru}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Poland</div>
-          <img src={pl}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Indonesia</div>
-          <img src={id}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Sweden</div>
-          <img src={se}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">France</div>
-          <img src={fr}></img>
-        </div>
-        <div id="element">
-          <div onClick={this.select1} className="name">Iceland</div>
-          <img src={is}></img>
-        </div>
-        
-        
       </div>
       
-
+      <div className="grid">
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[0].name}</div>
+          <img src={country[0].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[1].name}</div>
+          <img src={country[1].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[2].name}</div>
+          <img src={country[2].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[3].name}</div>
+          <img src={country[3].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[4].name}</div>
+          <img src={country[4].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[5].name}</div>
+          <img src={country[5].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[6].name}</div>
+          <img src={country[6].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[7].name}</div>
+          <img src={country[7].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[8].name}</div>
+          <img src={country[8].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[9].name}</div>
+          <img src={country[9].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[10].name}</div>
+          <img src={country[10].flag}></img>
+        </div>
+        <div id="element">
+          <div onClick={this.select1} className="name">{country[11].name}</div>
+          <img src={country[11].flag}></img>
+        </div>
+        
+      </div>
+      <footer>
       
+        <div>Copyrights 2022</div>
+        <div className="myName">ElijahMonj</div>
+        <a href="https://github.com/ElijahMonj/memory-card"><img src={gh} id="github"></img></a>
+        
       
+    </footer>
       </div>
     );
   }
